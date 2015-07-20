e905f34c63c716332b7d2a3baab5361decb9446d
nga.viet <cuonghh.2121989@gmail.com>
2015-07-20 11:15:06 +0700

    commit

--- addons/default/modules/tdesign/config/tdesign.php
+++ addons/default/modules/tdesign/config/tdesign.php
@@ -5,5 +5,6 @@ $config ['min_width_template_file'] = 480;
 $config ['min_height_template_file'] = 480;
 $config ['tdesign_upload_path_folder']=UPLOAD_PATH.'../design/temp';
 $config ['tdesign_upload_path_temp']=BASE_URL.'/uploads/design/temp';
+$config ['tdesign_image_url']=BASE_URL.'uploads/design/';
 $config ['tdesign_template_img_resize_width']=480;
 $config ['tdesign_template_img_resize_height']=480;
