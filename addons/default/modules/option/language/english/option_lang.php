8542f0f26f8edd4cfe85b1cf1f25118bd11ca2a1
nga.viet <cuonghh.2121989@gmail.com>
2015-07-26 10:08:18 +0700

    update

--- addons/default/modules/option/language/english/option_lang.php
+++
@@ -1,9 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-
-$lang['cp:na_option']                       = 'Options';
-$lang['option:option_name']					="Option Name";
-$lang['option:option_title']				="Options";
-$lang['option:option_position']="Position";
-$lang['option:option_type']="Type";
-$lang['option:attributes']="Attributes";
---
+++ addons/default/modules/template/views/admin/tables/templates.php
@@ -0,0 +1,42 @@
+
+	<table cellspacing="0">
+		<thead>
+			<tr>
+				<th width="20"><?php echo form_checkbox(array('name' => 'action_to_all', 'class' => 'check-all')) ?></th>
+				<th><?php echo lang('global:title') ?></th>
+				<th><?php echo lang('template:group') ?></th>
+				<th class="collapse"><?php echo lang('template:category_label') ?></th>
+				<th class="collapse"><?php echo lang('template:date_label') ?></th>
+			
+				<th><?php echo lang('template:status_label') ?></th>
+				<th width="180"><?php echo lang('global:actions') ?></th>
+			</tr>
+		</thead>
+		<tbody>
+			
+			<?php foreach ($templates as $template) : ?>
+				<tr>
+					<td><?php echo form_checkbox('action_to[]', $template->id_template) ?></td>
+					<td class="collapse"><?php  echo $template->name?></td>
+					<td class="collapse"><?php  echo $template->group_id?></td>
+					<td class="collapse"><?php  echo $template->cate_name?></td>
+					<td class="collapse"><?php echo format_date($template->timestamp) ?></td>				
+					<td><?php echo lang('template:'.$template->status.'_label') ?></td>
+					<td style="padding-top:10px;">
+                       
+                      
+						<a href="<?php echo site_url('admin/template/form/' . $template->id_template) ?>" title="<?php echo lang('global:edit')?>" class="button"><?php echo lang('global:edit')?></a>
+						<a href="<?php echo site_url('admin/template/delete/' . $template->id_template) ?>" title="<?php echo lang('global:delete')?>" class="button confirm"><?php echo lang('global:delete')?></a>
+					</td>
+				</tr>
+			<?php endforeach ?>
+		</tbody>
+	</table>
+
+	<?php  $this->load->view('admin/partials/pagination') ?>
+
+	<br>
+
+	<div class="table_action_buttons">
+		<?php $this->load->view('admin/partials/buttons', array('buttons' => array('delete', 'publish',"set_group"))) ?>
+	</div>
