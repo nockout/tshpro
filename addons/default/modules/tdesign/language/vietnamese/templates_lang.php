6fbdedc78d967827d026090986bff41ebb04501e
nockuot <VIETNGA@IPTV>
2015-07-30 06:59:40 +0700

    language

--- addons/default/modules/tdesign/language/vietnamese/templates_lang.php
+++ addons/default/modules/tdesign/language/vietnamese/templates_lang.php
@@ -1,6 +1,6 @@
 <?php
-$lang['design:manages_layer']                       = 'Manage Layers';
-$lang['design:add_your_image']						 = "Add Your Image ";
-$lang['design:add_your_own_text']						 = "Add Your Own Text ";
+$lang['design:manages_layer']                       = 'quản lý Layers';
+$lang['design:add_your_image']						 = "Thêm hình ảnh ";
+$lang['design:add_your_own_text']						 = "Thêm text ";
 $lang['design:invalid_mockup_size']						 = "Sorry! But the uploaded image size does not conform our indication of size.\\n Minimum size 1200x1200 and Maximum 3600x3600";
 $lang['design:add_mockup']					= 'Add Your Art';
