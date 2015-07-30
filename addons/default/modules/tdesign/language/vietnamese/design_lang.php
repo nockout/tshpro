26ffdd180f35019c26ad108aea4df0e38dd44a17
nockuot <VIETNGA@IPTV>
2015-07-30 22:55:08 +0700

    update

---
+++ addons/default/modules/tdesign/language/vietnamese/design_lang.php
@@ -0,0 +1,116 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$lang['cp:nav_design']                       = 'Thiết kế';
+$lang['design:create']						 = "Tạo mới Design ";
+$lang['design:management']						 = "Quản lý Designs ";
+$lang['design:design_title']						 = "New Design ";
+$lang['design:create_title']						 = "Tạo mới Design ";
+$lang['design:edit_title']						 = "chỉnh sửa Design ";
+$lang['design:currently_no_products']						 = "Không có design nào";
+//$lang['cp:nav_design']                       = 'Design';
+$lang['design:create']						 = "Tạo Style ";
+$lang['design:management']						 = "Quản lý style";
+$lang['design:design_title']						 = "Style mới ";
+$lang['design:create_title']						 = "Tạo Style ";
+$lang['design:edit_title']						 = "Edit Design ";
+$lang['design:currently_no_products']						 = "Không có style tại thời điểm này";
+$lang['design:design']                 = 'Design';
+$lang['design:designs']                   = 'Designs';
+$lang['design:style']                   = 'Styles';
+$lang['design:price']="Giá";
+// labels
+$lang['design:description_label']="Description";
+$lang['design:designed_label']                   = 'Tên';
+$lang['design:designed_label_alt']               = 'Designed at';
+$lang['design:written_by_label']				= 'Design by';
+$lang['design:description_label']="Miêu tả";
+$lang['design:designed_label']                   = 'Tên ';
+$lang['design:designed_label_alt']               = 'Ngày tạo';
+$lang['design:author_unknown']				= 'Unknown';
+$lang['design:keywords_label']				= 'Keywords';
+$lang['design:tagged_label']					= 'Tagged';
+$lang['design:category_label']                 = 'Danh mục';
+$lang['design:design_label']                     = 'Design';
+$lang['design:date_label']                     = 'Ngày tạo';
+$lang['design:date_at']                        = 'at';
+$lang['design:time_label']                     = 'Time';
+$lang['design:status_label']                   = 'Trạng thái';
+$lang['design:draft_label']                    = 'Draft';
+$lang['design:A_label']                   = 'hoạt động';
+$lang['design:D_label']                   = 'không hoạt động';
+$lang['design:A_label']                   = 'Kích hoạt';
+$lang['design:D_label']                   = 'Không kích hoạt';
+$lang['design:live_label']                     = 'Live';
+$lang['design:content_label']                  = 'Nội dung';
+$lang['design:options_label']                  = 'Lựa chọn';
+$lang['design:intro_label']                    = 'Giới thiêu';
+$lang['design:no_category_select_label']       = '-- None --';
+$lang['design:new_category_label']             = 'Thêm 1 danh mucj';
+$lang['design:subscripe_to_rss_label']         = 'Subscribe to RSS';
+$lang['design:all_Designs_label']                = 'Tất cả thiết kế';
+$lang['design:designs_of_category_suffix']       = ' Designs';
+$lang['design:rss_name_suffix']                = ' design';
+$lang['design:rss_category_suffix']            = ' design';
+$lang['design:author_name_label']              = 'Tác giả';
+$lang['design:read_more_label']                = 'Đọc thêm&nbsp;&raquo;';
+$lang['design:created_hour']                   = 'Giờ tạo';
+$lang['design:created_minute']                 = 'Phút tạo';
+$lang['design:comments_enabled_label']         = 'Comments Enabled';
+
+// titles
+$lang['design:create_title']                   = 'Add Design';
+$lang['design:edit_title']                     = 'Edit Design "%s"';
+$lang['design:archive_title']                 = 'Archive';
+$lang['design:designs_title']					= 'Designs';
+$lang['design:rss_Designs_title']				= 'design Designs for %s';
+$lang['design:design_title']					= 'Designs';
+$lang['design:description_title']					= 'Desciption';
+$lang['design:text_options']					= 'Text Options';
+$lang['design:enter_text']					= 'Enter text below';
+$lang['design:new_text']					= 'New Text';
+$lang['design:image_option']					= 'Image Options';
+$lang['design:font_size']					= 'Font size';
+$lang['design:select_font']					= 'Select Fonts';
+$lang['design:font_color']					= 'Font Colors';
+$lang['design:font']					= 'Fonts';
+$lang['design:apply_this_style']="Lưu style này";
+$lang['design:save_all_design']="Export tất cả style";
+
+$lang['design:upload_template']					= 'Upload Template';
+$lang['design:image_options']					= 'Image Options';
+
+
+$lang['design:collections']					= 'Collections';
+$lang['design:print']					= 'Print';
+$lang['design:preview']					= 'Preview';
+$lang['design:export']					= 'Export';
+$lang['design:help']="Help";
+$lang['design:text_help']="Click To edit Text or Click on the Text(font size, Font, color)";
+$lang['design:remove_text_help']="Click To remove text after confirmation";
+$lang['design:text_resize_image']="Drag To resize images";
+$lang['design:add_template_here']="Add template here";
+// messages
+$lang['design:disabled_after'] 				= 'Designing comments after %s has been disabled.';
+$lang['design:no_Designs']                      = 'There are no Designs.';
+$lang['design:subscripe_to_rss_desc']          = 'Get Designs straight away by subscribing to our RSS feed. You can do this via most popular e-mail clients, or try <a href="http://reader.google.co.uk/">Google Reader</a>.';
+$lang['design:currently_no_Designs']          = 'There are no Designs at the moment.';
+$lang['design:design_add_success']            = 'The Design "%s" was added.';
+$lang['design:design_add_error']              = 'An error occured.';
+$lang['design:edit_success']                   = 'The Design "%s" was updated.';
+$lang['design:edit_error']                     = 'An error occurred.';
+$lang['design:publish_success']                = 'The Design  has been published.';
+$lang['design:mass_publish_success']           = 'The Designs  have been published.';
+$lang['design:publish_error']                  = 'No Designs were published.';
+$lang['design:delete_success']                 = 'The Design  has been deleted.';
+$lang['design:mass_delete_success']            = 'The Designs  have been deleted.';
+$lang['design:delete_error']                   = 'No Designs were deleted.';
+$lang['design:already_exist_error']            = 'A Design with this URL already exists.';
+$lang['design:template_upload_sucessfull']            = 'Your template file uploaded successfully';
+$lang['design:disabled_after'] 				= 'Designing comments after %s has been disabled.';
+$lang['design:twitter_Designed']                 = 'Designed "%s" %s';
+$lang['design:no_origin_size_set']                  = 'No original size be set.Pleas,contact administrator to repair it ';
+
+// date
+$lang['design:archive_date_format']		= "%B %Y";
+$lang['design:your_design']='Designs';
