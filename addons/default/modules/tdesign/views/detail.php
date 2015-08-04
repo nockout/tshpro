feefaeeeb5ef2a18da7b454b658843eb5493d0cf
VN <VN@VNCOMPUTER>
2015-08-04 21:06:07 +0700

    detail

--- addons/default/modules/tdesign/views/detail.php
+++ addons/default/modules/tdesign/views/detail.php
@@ -8,10 +8,10 @@
 					<div class="productFrame">
 						<span class="priceshow"><?php echo $product->list_price?></span>
 						<img width="13" height="8" class="btm-s" src="">
-					
+			
 					<?php if(!empty($product->image)):?>
 						<?php $first=array_shift($product->image);?>
-						<?php //echo $first;die;?>
+				
 						<a data-theimage="<?php ?>" data-target="#imageModal" data-toggle="modal">
 							<img width="651" height="651" class="img-responsive lg_view" alt="<?php echo $product->product?>" src="<?php echo $first?>">
 					
@@ -52,91 +52,21 @@
 							
 						</select>
 					</div>
-
-					
+				
 
 					<div class="clearfix"></div>
 					
-					<form method="post" action="" id="formAddToCart">
-						
-						<input type="hidden" value="CFID=49528770&amp;CFTOKEN=63ae474e4846a7a1-36002073-F666-65D2-D59C8C14E07D1736" name="tokensVar">
-						<input type="hidden" value="60749949" name="mockupID">
-						<input type="hidden" value="Jeb Bush 2016" name="title">
-						<input type="hidden" value="For those voting and support Jeb Bush in the 2016 Presidential race!" name="description">
-						<input type="hidden" value="Jeb-Bush-2016.jpg" name="proofImage">
-						<input type="hidden" value="2015/07/29/" name="proofImageDir">
-						<input type="hidden" value="8" name="MockupTypeID">
-						<input type="hidden" value="4882544" name="MockupGroup">
-						<input type="hidden" value="" name="DGfileFront">
+					
+				
+					
+					 <?php echo form_open('cart/add_to_cart', 'class="form-horizontal"');?>
 							
-						
-						<input type="hidden" value="MSIgTydSPj1NS09UXjZAMFlPKkU4V0REIAo=" name="ArtOwner">	
-						
-						<input type="hidden" value="LSJWVydTWlE3XyhQQjZQKSlZUCAgCg==" name="isExempt">
-						
-						<input type="hidden" value="Pjw1PjRUKVUnWyo9QCM1PStZPi01RjEiKzs9Oz0iTyk+TCMxOzZfRyAK" name="prodURL">
-						
-						<input type="hidden" value="1" name="Quantity">
-			
+						<input type="hidden" name="cartkey" value="<?php echo $this->session->flashdata('cartkey');?>" />
+                   		<input type="hidden" name="id" value="<?php echo $product->product_id?>"/>
 						<div class="form-group">
-							
-							<button style="background:none; border:none; text-align:left; padding:8px 0px 8px 0px;" class=" text-primary" data-target="#sizeModal" type="button" data-toggle="modal">Sizing Chart</button>
-						
-								
-							<div data-placement="top" data-toggle="popover" class="size-select">
-							
-							
-							
-								
-								
-								<div data-toggle="buttons" class="btn-group">
-								
-								
-										<label class="btn btn-default" for="S">
-											<input type="radio" value="S_MSJATzhTPl1NS09UXjZAMFlPKkU4V0REIAo=" autocomplete="off" data-se="0" name="size" id="S">S
-										</label>
-									
-										<label class="btn btn-default" for="M">
-											<input type="radio" value="M_MSJATzhTPl1NS09UXjZAMFlPKkU4V0REIAo=" autocomplete="off" data-se="0" name="size" id="M">M
-										</label>
-									
-										<label class="btn btn-default" for="L">
-											<input type="radio" value="L_MSJATzhTPl1NS09UXjZAMFlPKkU4V0REIAo=" autocomplete="off" data-se="0" name="size" id="L">L
-										</label>
-									
-										<label class="btn btn-default" for="XL">
-											<input type="radio" value="XL_MSJATzhTPl1NS09UXjZAMFlPKkU4V0REIAo=" autocomplete="off" data-se="0" name="size" id="XL">XL
-										</label>
-									
-									<label class="btn btn-default" title="+ $3" data-placement="top" for="XXL">
-									<input type="radio" value="XXL_MSIwIzhTPl1NS09UXjZAMFlPKkU4V0REIAo=" data-se="1" name="size" id="XXL">2X
-									</label>
-								
-									<label class="btn btn-default" title="+ $3" data-placement="top" for="XXXL">
-									<input type="radio" value="XXXL_MSIwIzhTPl1NS09UXjZAMFlPKkU4V0REIAo=" data-se="1" name="size" id="XXXL">3X
-									</label>
-								
-									<label class="btn btn-default" title="+ $3" data-placement="top" for="XXXXL">
-									<input type="radio" value="XXXXL_MSIwIzhTPl1NS09UXjZAMFlPKkU4V0REIAo=" data-se="1" name="size" id="XXXXL">4X
-									</label>
-								
-								
+									<button class="btn btn-lg btn-block btn-xxl btn-success btn-bright  hidden-xs" name="submit" type="submit">Order &nbsp;&nbsp;<i class="fa fa-caret-right"></i></button>
 								</div>
-								
-							</div>
-						</div>
-						
-						<a name="sizeAnch"></a>
-						
-						
-						
-								
-		
-						
-							
-						
-
-					</form>
+					<?php echo form_close()?>
 					
 					<div class="visible-xs">
 					
@@ -195,263 +125,9 @@
 
 	
 	
-	<style>
-		#showRecentlyViewed .titlebox { display: none; }
-	</style>
-	<div style="display:none;" id="showRecentlyViewed">
-		<h4>Recently Viewed</h4>
-		<div class="bx-wrapper" style=""><div class="bx-viewport" style="width: 100%; overflow: hidden; position: relative;"><div class="slideshow2 slidewrap" style="width: 215%; position: relative;"></div></div><div class="bx-controls bx-has-pager bx-has-controls-direction"><div class="bx-pager"></div><div class="bx-controls-direction"><a href="" class="bx-prev">Prev</a><a href="" class="bx-next">Next</a></div></div></div>
-		<div class="clearfix"></div>
-		<br><br>
-	</div> 
+	
 
-			<h4>You might also like</h4>
-			<div class="bx-wrapper" style="max-width: 1460px;"><div class="bx-viewport" style="width: 100%; overflow: hidden; position: relative; height: 190px;"><div class="slideshow1 slidewrap" style="width: 1215%; position: relative; transition-duration: 0s; transform: translate3d(-1470px, 0px, 0px);"><div class="slide bx-clone" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="No One Owes You" href="no-one-owes-shirt.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_work1.jpg">
-									</a>
-								</div>
-								
-
-							</div><div class="slide bx-clone" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="stupid people in large groups" href="stupid-people-in-large-groups.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_SD-LargeGroups1.jpg">
-									</a>
-								</div>
-								
-
-							</div><div class="slide bx-clone" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="Freedom" href="freedom-flag5.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_freedom6.jpg">
-									</a>
-								</div>
-								
-
-							</div><div class="slide bx-clone" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="Dont Be Upset" href="dont-be-upset-tee.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_upset2.jpg">
-									</a>
-								</div>
-								
-
-							</div><div class="slide bx-clone" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="I didnt go to Harvard, I went to Vietnam" href="I-didnt-go-to-Harvard-I-went-to-Vietnam.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/2014/10/15/md_I-didnt-go-to-Harvard-I-went-to-Vietnam.jpg">
-									</a>
-								</div>
-								
-
-							</div><div class="slide bx-clone" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="Veteran meaning" href="Veteran-meaning.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/2014/10/18/md_Veteran-meaning.jpg">
-									</a>
-								</div>
-								
-
-							</div><div class="slide bx-clone" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="Freedom" href="freedom-flag1.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_freedom1.jpg">
-									</a>
-								</div>
-								
-
-							</div>
-				
-				
-						
-							
-							<div class="slide" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="Why An AR-15?" href="Why-An-AR-15.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_Why-An-AR-15.jpg">
-									</a>
-								</div>
-								
-
-							</div>
-		
-							
-						
-							
-							<div class="slide" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="I Work Hard" href="welfare-depends-on-me.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/2014/07/09/md_work hard.jpg">
-									</a>
-								</div>
-								
-
-							</div>
-		
-							
-						
-							
-							<div class="slide" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="Gun Control" href="Gun-Control.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_Gun-Control.jpg">
-									</a>
-								</div>
-								
-
-							</div>
-		
-							
-						
-							
-							<div class="slide" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="No One Owes You" href="no-one-owes-shirt.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_work1.jpg">
-									</a>
-								</div>
-								
-
-							</div>
-		
-							
-						
-							
-							<div class="slide" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="stupid people in large groups" href="stupid-people-in-large-groups.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_SD-LargeGroups1.jpg">
-									</a>
-								</div>
-								
-
-							</div>
-		
-							
-						
-							
-							<div class="slide" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="Freedom" href="freedom-flag5.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_freedom6.jpg">
-									</a>
-								</div>
-								
-
-							</div>
-		
-							
-						
-							
-							<div class="slide" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="Dont Be Upset" href="dont-be-upset-tee.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_upset2.jpg">
-									</a>
-								</div>
-								
-
-							</div>
-		
-							
-						
-							
-							<div class="slide" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="I didnt go to Harvard, I went to Vietnam" href="I-didnt-go-to-Harvard-I-went-to-Vietnam.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/2014/10/15/md_I-didnt-go-to-Harvard-I-went-to-Vietnam.jpg">
-									</a>
-								</div>
-								
-
-							</div>
-		
-							
-						
-							
-							<div class="slide" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="Veteran meaning" href="Veteran-meaning.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/2014/10/18/md_Veteran-meaning.jpg">
-									</a>
-								</div>
-								
-
-							</div>
-		
-							
-						
-							
-							<div class="slide" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="Freedom" href="freedom-flag1.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_freedom1.jpg">
-									</a>
-								</div>
-								
-
-							</div>
-		
-							
-			<div class="slide bx-clone" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="Why An AR-15?" href="Why-An-AR-15.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_Why-An-AR-15.jpg">
-									</a>
-								</div>
-								
-
-							</div><div class="slide bx-clone" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="I Work Hard" href="welfare-depends-on-me.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/2014/07/09/md_work hard.jpg">
-									</a>
-								</div>
-								
-
-							</div><div class="slide bx-clone" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="Gun Control" href="Gun-Control.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_Gun-Control.jpg">
-									</a>
-								</div>
-								
-
-							</div><div class="slide bx-clone" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="No One Owes You" href="no-one-owes-shirt.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_work1.jpg">
-									</a>
-								</div>
-								
-
-							</div><div class="slide bx-clone" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="stupid people in large groups" href="stupid-people-in-large-groups.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_SD-LargeGroups1.jpg">
-									</a>
-								</div>
-								
-
-							</div><div class="slide bx-clone" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="Freedom" href="freedom-flag5.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_freedom6.jpg">
-									</a>
-								</div>
-								
-
-							</div><div class="slide bx-clone" style="float: left; list-style: outside none none; position: relative; width: 200px; margin-right: 10px;">
-								<div class="product_FrameLG">
-									<a border="0" title="Dont Be Upset" href="dont-be-upset-tee.html">
-										<img width="200" height="224" alt="" src="//images.sunfrogshirts.com/md_upset2.jpg">
-									</a>
-								</div>
-								
-
-							</div></div></div><div class="bx-controls bx-has-pager bx-has-controls-direction"><div class="bx-pager bx-default-pager"><div class="bx-pager-item"><a class="bx-pager-link active" data-slide-index="0" href="">1</a></div><div class="bx-pager-item"><a class="bx-pager-link" data-slide-index="1" href="">2</a></div></div><div class="bx-controls-direction"><a href="" class="bx-prev">Prev</a><a href="" class="bx-next">Next</a></div></div></div>
+			<?php //echo $this->load->view("alsolike")?>
 		
 		<div data-colorscheme="light" data-width="100%" data-order-by="reverse_time" data-numposts="5" data-href="https://www.sunfrog.com/Jeb-Bush-2016.html" class="fb-comments fb_iframe_widget fb_iframe_widget_fluid" fb-xfbml-state="rendered"><span style="height: 176px; width: 1140px;"><iframe id="f30ec97af312fbe" name="f1bd557df0de048" scrolling="no" style="border: medium none; overflow: hidden; height: 176px; width: 1140px;" title="Facebook Social Plugin" class="fb_ltr" src="https://www.facebook.com/plugins/comments.php?api_key=842113255878673&amp;channel_url=https%3A%2F%2Fs-static.ak.facebook.com%2Fconnect%2Fxd_arbiter%2F4B2NplaqNF3.js%3Fversion%3D41%23cb%3Df292d8b60a93854%26domain%3Dwww.sunfrog.com%26origin%3Dhttps%253A%252F%252Fwww.sunfrog.com%252Ff549dfdf42942%26relation%3Dparent.parent&amp;colorscheme=light&amp;href=https%3A%2F%2Fwww.sunfrog.com%2FJeb-Bush-2016.html&amp;locale=en_US&amp;numposts=5&amp;order_by=reverse_time&amp;sdk=joey&amp;skin=light&amp;version=v2.3&amp;width=100%25"></iframe></span></div>
 	
