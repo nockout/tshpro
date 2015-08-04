f6c5b386e798b5e7fca4f28c1471fa1072207b9d
VN <VN@VNCOMPUTER>
2015-08-04 21:24:22 +0700

    update image

--- addons/default/modules/tdesign/views/detail.php
+++ addons/default/modules/tdesign/views/detail.php
@@ -9,8 +9,15 @@
 						<span class="priceshow"><?php echo $product->list_price?></span>
 						<img width="13" height="8" class="btm-s" src="">
 			
-					<?php if(!empty($product->image)):?>
-						<?php $first=array_shift($product->image);?>
+					<?php 
+					$extra=$product->extra;
+					if($extra){
+						$extra=unserialize($extra);
+					}
+					//echo "<pre>";
+					//print_r($extra);die;
+					if(!empty($extra['image'])):?>
+						<?php $first=array_shift($extra['image']);?>
 				
 						<a data-theimage="<?php ?>" data-target="#imageModal" data-toggle="modal">
 							<img width="651" height="651" class="img-responsive lg_view" alt="<?php echo $product->product?>" src="<?php echo $first?>">
