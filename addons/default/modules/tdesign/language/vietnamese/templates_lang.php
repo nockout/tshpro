4d9f73deda217a8752407625c440377b1c22acf8
VN <VN@VNCOMPUTER>
2015-08-04 21:04:24 +0700

    language

--- addons/default/modules/tdesign/language/vietnamese/templates_lang.php
+++ addons/default/modules/tdesign/language/vietnamese/templates_lang.php
@@ -4,8 +4,10 @@ $lang['template:add_your_image']						 = "Thêm hình";
 $lang['template:add_your_own_text']						 = "Thêm text ";
 $lang['template:invalid_mockup_size']						 = "Sorry! But the uploaded image size does not conform our indication of size.\\n Minimum size 1200x1200 and Maximum 3600x3600";
 $lang['template:add_mockup']					= 'Thêm logo';
-$lang['template:choose_product']='Đổi sản phẩm ';
+
+$lang['template:choose_product']='Chọn sản phẩm ';
 $lang['template:manages_layer']='Layer';
 $lang['template:enter_text']='Thêm text';
 $lang['template:functions']='Arts';
 
+$lang['template:change_product']='Đổi sản phẩm ';
