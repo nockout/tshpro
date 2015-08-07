4eb62c8cb736625847df5c9b69e799ae38459b52
nockout <cuonghh.2121989@gmail.com>
2015-08-07 22:56:13 +0700

    update

--- addons/default/modules/order/views/admin/partials/download.php
+++ addons/default/modules/order/views/admin/partials/download.php
@@ -3,9 +3,10 @@
 
 	<?php if(!empty($arts)):?>
 		<ul>
-				<?php foreach ($arts as $art)?>
-				<li><a download href="<?php echo $art?>" target="_blank"><img style="width: 120px;height:120px" src="<?php echo $art?>" /></a></li>
-				<?php ?>
+				<?php foreach ($arts as $art):?>
+				<li><a download href="<?php echo $art?>" target="_blank">
+				<img style="width: 120px;height:120px" src="<?php echo $art?>" /></a></li>
+				<?php endforeach;?>
 		</ul>
 		<?php endif;?>
 </fieldset>
