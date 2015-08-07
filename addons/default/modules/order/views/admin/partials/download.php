45535c8c2d1b34554fa53a1141458e6acc15acf5
nockout <cuonghh.2121989@gmail.com>
2015-08-07 22:14:41 +0700

    update

--- addons/default/modules/order/views/admin/partials/download.php
+++ addons/default/modules/order/views/admin/partials/download.php
@@ -1,11 +1,11 @@
 <fieldset id="filters">
 	<legend><?php echo lang('design:arts') ?></legend>
 
-	<?php if(!empty($arts))?>
+	<?php if(!empty($arts)):?>
 		<ul>
 				<?php foreach ($arts as $art)?>
 				<li><a download href="<?php echo $art?>" target="_blank"><img src="<?php echo $art?>" /></a></li>
 				<?php ?>
 		</ul>
-
+		<?php endif;?>
 </fieldset>
